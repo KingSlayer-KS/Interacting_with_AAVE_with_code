489398880c0351f8ff29de17ee028c1a0edce8aa
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-03-30 19:59:47 +0530

    updated

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -1,25 +1,30 @@
-
 from web3 import Web3
 
 from scripts.helpful_scripts import get_account
-from brownie import  interface,config,network
+from brownie import interface, config, network
+
 
 def convert_to_WETH_or_WAVAX():
-    #Address
-    account=get_account()
-    #ABI
-    if network.show_active() in ["avax-main-fork","avax-main","avax-test"]:
-        weth=interface.IWavax(config["networks"][network.show_active()]["wavax_token"])
-        tx=weth.deposit({"from":account,"value": 0.1 * 10 ** 18})
-    elif network.show_active() in ["polygon-main-fork","polygon-main","polygon-test"]:
-        weth=interface.IWmatic(config["networks"][network.show_active()]["wmatic_token"])
-        tx=weth.deposit({"from":account,"value": 0.1 * 10 ** 18})
+    # Address
+    account = get_account()
+    # ABI
+    if network.show_active() in ["avax-main-fork", "avax-main", "avax-test"]:
+        weth = interface.IWavax(
+            config["networks"][network.show_active()]["wavax_token"]
+        )
+        tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    elif network.show_active() in ["polygon-main-fork", "polygon-main", "polygon-test"]:
+        weth = interface.IWmatic(
+            config["networks"][network.show_active()]["wmatic_token"]
+        )
+        tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
     else:
-        weth=interface.IWeth(config["networks"][network.show_active()]["weth_token"])
-        tx=weth.deposit({"from":account,"value": 0.1 * 10 ** 18})
+        weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+        tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
     tx.wait(1)
     print("wETH recieved")
     return tx
+
 
 def main():
     convert_to_WETH_or_WAVAX()
